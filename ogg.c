22346e090963efcbcc84d8ecb35d1220110a856a
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2005-01-05 09:34:51 +0000

    Merged from trunk: Fixed setting sound_params.
    git-svn-id: svn://daper.net/moc/branches/2.1@854 910807d9-36e0-0310-a014-e9ea483e2ba4

--- ogg.c
+++ ogg.c
@@ -149,6 +149,7 @@ static int ogg_decode (void *void_data, char *buf, int buf_len,
 	int ret;
 	int current_section;
 	int bitrate;
+	vorbis_info *info;
 
 	while (1) {
 		ret = ov_read(&data->vf, buf, buf_len, 0, 2, 1,
@@ -162,17 +163,16 @@ static int ogg_decode (void *void_data, char *buf, int buf_len,
 		}
 		
 		if (current_section != data->last_section) {
-			vorbis_info *info;
-
 			logit ("section change or first section");
 			
 			data->last_section = current_section;
-			info = ov_info (&data->vf, -1);
-			assert (info != NULL);
-			sound_params->channels = info->channels;
-			sound_params->rate = info->rate;
-			sound_params->format = 2;
 		}
+
+		info = ov_info (&data->vf, -1);
+		assert (info != NULL);
+		sound_params->channels = info->channels;
+		sound_params->rate = info->rate;
+		sound_params->format = 2;
 
 		/* Update the bitrate information */
 		bitrate = ov_bitrate_instant (&data->vf);

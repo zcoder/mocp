2a652b4d17cf8de64e4fe3a9b387ad51c81aefc5
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2005-02-15 18:00:19 +0000

    Fixed marking file.
    git-svn-id: svn://daper.net/moc/branches/2.2@1007 910807d9-36e0-0310-a014-e9ea483e2ba4

--- interface.c
+++ interface.c
@@ -1104,7 +1104,7 @@ static void mark_file (const char *file)
 				&& (i = plist_find_fname(playlist, file))
 				!= -1)
 			menu_mark_plist_item (playlist_menu, i);
-		else if (curr_plist_menu
+		if (curr_plist_menu
 				&& (i = plist_find_fname(curr_plist, file))
 				!= -1)
 			menu_mark_plist_item (curr_plist_menu, i);
